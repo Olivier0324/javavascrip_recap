9bd141ca89c048880fc77a3c2f5f0959d8800e5d
Olivier0324 <techies.dev24@gmail.com>
2025-05-29 20:53:17 +0200

    feat: added simple calculator

--- calculator.js
+++ calculator.js
@@ -1,28 +1,39 @@
 let userInput = require("readline-sync");
-let firstNum = parseInt(userInput.question("Enter the first number: "));
-let secondNum = parseInt(userInput.question("Enter the second number: "));
-let operatorChoice = String(userInput.question("Choose operator (+, -, *, /): "));
 let result = null;
-if (operatorChoice === "+") {
-    result = firstNum + secondNum;
-    console.log(`${firstNum} ${operatorChoice} ${secondNum} = ${result}`);
-}
-else if (operatorChoice === "-") {
-    result = firstNum - secondNum;
-    console.log(`${firstNum} ${operatorChoice} ${secondNum} = ${result}`);
-}
-else if (operatorChoice === "*") {
-    result = firstNum * secondNum;
-    console.log(`${firstNum} ${operatorChoice} ${secondNum} = ${result}`);
-}
-else if (operatorChoice === "/") {
-    if (secondNum === 0) {
-        console.log(`Error: impossible to devide ${firstNum} with ${secondNum} `);
-    } else {
-        result = firstNum / secondNum;
+
+let userCommand;
+do {
+    userCommand = userInput.question("Type a command (or 'exit' to quit): ").toLowerCase(); 
+    let firstNum = parseInt(userInput.question("Enter the first number: "));
+    let secondNum = parseInt(userInput.question("Enter the second number: "));
+    let operatorChoice = String(userInput.question("Choose operator (+, -, *, /): "));
+    if (operatorChoice === "+") {
+        result = firstNum + secondNum;
         console.log(`${firstNum} ${operatorChoice} ${secondNum} = ${result}`);
     }
+    else if (operatorChoice === "-") {
+        result = firstNum - secondNum;
+        console.log(`${firstNum} ${operatorChoice} ${secondNum} = ${result}`);
+    }
+    else if (operatorChoice === "*") {
+        result = firstNum * secondNum;
+        console.log(`${firstNum} ${operatorChoice} ${secondNum} = ${result}`);
+    }
+    else if (operatorChoice === "/") {
+        if (secondNum === 0) {
+            console.log(`Error: impossible to devide ${firstNum} with ${secondNum} `);
+        } else {
+            result = firstNum / secondNum;
+            console.log(`${firstNum} ${operatorChoice} ${secondNum} = ${result}`);
+        }
+    }
+    else {
+        console.log("invalid operator, try between +,-,*,/ 🙏")
+    }
+
+    if (userCommand === 'exit') {
+        console.log("Exiting application.");
+        break; 
+    } 
 }
-else {
-    console.log("invalid operator, try between +,-,*,/ 🙏")
-}
+while (true);
