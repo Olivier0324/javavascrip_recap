a456447e074fecf5cdc445d004c571160afdf20f
Olivier0324 <techies.dev24@gmail.com>
2025-05-29 15:05:10 +0200

    feat: added Guessing_number_game file

--- guessing_number_game.js
+++ guessing_number_game.js
@@ -1,1 +1,21 @@
- 
+
+const userInput = require('readline-sync');
+
+let computerChoice = Math.floor(Math.random() * 10) + 1;
+let userChoice;
+let count = 0;
+
+console.log("I'm thinking of a number between 1 and 10.");
+
+while (userChoice !== computerChoice) {
+    userChoice = parseInt(userInput.question("Take a guess: "));
+    count++;
+
+    if (userChoice < computerChoice) {
+        console.log("Too low.");
+    } else if (userChoice > computerChoice) {
+        console.log("Too high.");
+    } else {
+        console.log("Correct! You guessed the number in " + count + " attempts.");
+    }
+}
